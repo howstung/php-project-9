24c56f578a624943f58140757fdbfe7ad21fb63c
howstung <howstung@gmail.com>
2024-02-12 23:55:33 +0300

    Create Twig templates

--- public/index.php
+++ public/index.php
@@ -1,16 +1,53 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+use DI\Container;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+// Create Container
+$container = new Container();
+AppFactory::setContainer($container);
 
-$app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Main of Page Analizer");
-    return $response;
+// Set view in Container
+$container->set('view', function () {
+    return Twig::create(__DIR__ . '/../templates', ['cache' => false]);
 });
 
+// Set Flash messages in Container
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
+});
+
+// Create App
+$app = AppFactory::create();
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::createFromContainer($app));
+
+
+// Define Main route
+$app->get('/', function ($request, $response, $args) {
+    $params = [
+        'title' => 'Анализатор страниц',
+        'menu' => [
+            'main' => [
+                'name' => 'Главная',
+                'link' => '/',
+                'active' => true
+            ],
+            'sites' => [
+                'name' => 'Сайты',
+                'link' => '/urls',
+            ],
+        ]
+    ];
+
+    return $this->get('view')->render($response, 'index.twig', $params);
+})->setName('main');
+
+
+// Run app
 $app->run();
